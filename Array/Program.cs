030461abff9558c9a9980f02347ba4fe1f1b7c57
fedya005 <verdikhanov01@bk.ru>
2024-02-17 19:06:10 +0300

    .

---
+++ Array/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(',');
+
+        string[] resultArray = FilterStrings(inputArray);
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(string.Join(", ", resultArray));
+    }
+
+    static string[] FilterStrings(string[] inputArray)
+    {
+        int count = 0;
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                resultArray[index] = inputArray[i];
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
